be19aef752b8221309abeb21ee7eb9c8ae9cb07a
Yohannes Getachew <demisgech@gmail.com>
2026-03-19 10:40:40 +0300

    Build theme switch

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import { ThemeSwitch } from "./components/ui";
+
 const App = () => {
   return (
     <div>
       <h1 className="text-2xl text-red-500">Hello react</h1>
       <button className="btn btn-primary">Button</button>
+      <ThemeSwitch />
     </div>
   );
 };
---
+++ src/components/ui/ThemeSwitch.tsx
@@ -0,0 +1,13 @@
+import { LuSun, LuMoon } from "react-icons/lu";
+
+const ThemeSwitch = () => {
+  return (
+    <label className="btn btn-ghost btn-circle swap swap-rotate">
+      <input type="checkbox" className="theme-controller" value="dracula" />
+      <LuSun className="swap-off h-6 w-6" />
+      <LuMoon className="swap-on h-6 w-6" />
+    </label>
+  );
+};
+
+export default ThemeSwitch;
