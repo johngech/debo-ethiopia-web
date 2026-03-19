4e803d64e89a5ccece6248a31b26a9474d365e54
Yohannes Getachew <demisgech@gmail.com>
2026-03-19 09:47:01 +0300

    Set up: configure daisyui

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ const App = () => {
   return (
     <div>
       <h1 className="text-2xl text-red-500">Hello react</h1>
+      <button className="btn btn-primary">Button</button>
     </div>
   );
 };
